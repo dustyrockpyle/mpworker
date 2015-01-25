b93d75b1a124327ae63545ddafcf6249e6141068
dustyrockpyle <dustyrockpyle@gmail.com>
2015-01-24 23:31:04 -0500

    Updated setup.py and setup.cfg for pypi.

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from distutils.core import setup
+import os
+
 
 setup(
     name='mpworker',
     version='0.1',
-    packages=['mpworker'],
+    description='Easy to use stateful multiprocessing. Asyncio compatible.',
     url='https://github.com/dustyrockpyle/mpworker',
+    download_url='https://github.com/dustyrockpyle/mpworker/tarball/0.1',
     license='MIT',
     author='Dustin Pyle',
     author_email='dustyrockpyle@gmail.com',
-    description='Easy to use stateful multiprocessing. Asyncio compatible.',
-    download_url='https://github.com/dustyrockpyle/mpworker/tarball/0.1'
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    packages=['mpworker'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+    ],
 )
