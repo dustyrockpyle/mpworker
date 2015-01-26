1acdde168e0e3eed9f5d51f686c79f2efe4525ef
dustyrockpyle <dustyrockpyle@gmail.com>
2015-01-26 18:18:00 -0500

    Fixed for pip.

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from distutils.core import setup
-import os
-
 
 setup(
     name='mpworker',
-    version='0.1',
+    version='0.1.1',
     description='Easy to use stateful multiprocessing. Asyncio compatible.',
     url='https://github.com/dustyrockpyle/mpworker',
     download_url='https://github.com/dustyrockpyle/mpworker/tarball/v0.1',
     license='MIT',
     author='Dustin Pyle',
     author_email='dustyrockpyle@gmail.com',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     packages=['mpworker'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
